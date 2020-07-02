bd7d37b4595f2fc21fedc393a47046e6be6d3df0
jfahler <jfahler@gmail.com>
2020-07-02 08:09:05 -0500

    HTML-CSS-JS project - started with core HTML, CSS, JS parts including essay form

---
+++ essayHelper.js
@@ -0,0 +1,5 @@
+function newEssay() {
+    var context = document.getElementById("contextInput").value; 
+    var thesis = document.getElementById("thesisInput").value; 
+    console.log(context + thesis); 
+}
